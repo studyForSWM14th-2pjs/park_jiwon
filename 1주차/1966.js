c1b730e29ff4af247fa73f49c835490276099eb4
Jiwon <raipen@knu.ac.kr>
2023-01-09 22:43:41 +0900

    update 1966

--- 1주차/1966.js
+++ 1주차/1966.js
@@ -1,22 +1,17 @@
 const fs = require('fs');
 const [T, ...input] = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
-const pop = priorityCount => queue => {
-    while (priorityCount.reduce((a, c, i) => a + (i > queue[0].data ? c : 0), 0) > 0)   // 자기보다 우선순위가 높은 문서가 남아있는지 확인
+const pop = queue => {
+    while (queue.find(e => e.data > queue[0].data)) // 우선순위가 높은 문서가 남아있다면
         queue.push(queue.shift());  // 우선순위가 높은 문서가 남아있다면 큐를 회전시킨다.
-    priorityCount[queue[0].data]--; // 우선순위가 높은 문서를 빼내므로 우선순위 카운트를 감소시킨다.
     return queue.shift();  // 우선순위가 높은 문서를 빼낸다.
 }
 
 for (let i = 0; i < T; i++) {
     let M = input[i * 2].split(" ")[1];
-    let queue = input[i * 2 + 1].split(" ");
-    let priorityCount = Array.from({ length: 10 }, () => 0); // 우선순위 카운트
-    queue.forEach(e => priorityCount[e]++); // 우선순위 카운트
-    popByPriority = pop(priorityCount); // 우선순위에 따라 문서를 빼내는 함수
-    queue = queue.map((q, i) => { return { index: i, data: q } }); // 큐에 인덱스를 추가
+    let queue = input[i * 2 + 1].split(" ").map((q, i) => { return { index: i, data: q } }); // 큐에 인덱스를 추가
     let count = 1;
-    while (popByPriority(queue).index != M) // M번째 문서가 나올 때까지 반복
+    while (pop(queue).index != M) // M번째 문서가 나올 때까지 반복
         count++;
     console.log(count);
 }
--- 1주차/1966short.js
+++ 1주차/1966short.js
@@ -1,9 +1,6 @@
-f=require('fs');
-[T,...I]=f.readFileSync("/dev/stdin").toString().trim().split("\n");
-for(i=0;i<T;i++){
-    M=I[i*2].split(" ")[1];q=I[i*2+1].split(" ").map((q,i)=>[q,i]);
-    p=Array.from({length:10},e=>0);
-    q.forEach(e=>p[e[0]]++);
-    o=q=>{while(p.reduce((a,c,i)=>a+(i>q[0][0]?c:0),0)>0)q.push(q.shift());p[q[0][0]]--;return q.shift();};
-    c=1;while(o(q)[1]!=M)c++;console.log(c);
-}
+(require('fs').readFileSync("/dev/stdin")+"").trim().split(`
+`).map(e=>e.split(" ")).map((e,i,a)=>{if(i%2){M=e[1]
+q=a[i+1].map((q,i)=>[q,i])
+c=0
+do while(q.find(e=>e[0]>q[0][0]))q.push(q.shift())
+while(c++,q.shift()[1]!=M)console.log(c)}})
